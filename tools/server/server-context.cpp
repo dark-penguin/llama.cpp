368caf763879de72a5c41828705f3d0e7fde7eb9
darkpenguin <darkpenguin@posteo.de>
2026-06-06 21:16:31 +0400

    More fixes

--- tools/server/server-context.cpp
+++ tools/server/server-context.cpp
@@ -73,6 +73,9 @@ struct server_slot {
     llama_context * ctx_tgt = nullptr;
     llama_context * ctx_dft = nullptr;
 
+    // context removal type for this slot
+    common_context_seq_rm_type ctx_seq_rm_type = COMMON_CONTEXT_SEQ_RM_TYPE_NO;
+
     // multimodal
     mtmd_context * mctx = nullptr;
 
@@ -566,6 +569,7 @@ struct server_slot {
 
         other.prompt = prompt.clone();
         other.init_sampler();
+        other.ctx_seq_rm_type = ctx_seq_rm_type;
     }
 };
 
@@ -1196,6 +1200,7 @@ private:
         // initialize slots
         for (int i = 0; i < params_base.n_parallel; i++) {
             slots.emplace_back();
+            slots.back().ctx_seq_rm_type = ctx_tgt_seq_rm_type;
         }
 
         // try speculative decoding
@@ -3109,8 +3114,8 @@ private:
                     // - the model uses SWA (and we are not using `swa_full`)
                     // - the model supports partial sequence removal but only up to a fixed bound
                     do_checkpoint = do_checkpoint && (
-                            ctx_tgt_seq_rm_type == COMMON_CONTEXT_SEQ_RM_TYPE_FULL ||
-                            ctx_tgt_seq_rm_type == COMMON_CONTEXT_SEQ_RM_TYPE_RS ||
+                            slot.ctx_seq_rm_type == COMMON_CONTEXT_SEQ_RM_TYPE_FULL ||
+                            slot.ctx_seq_rm_type == COMMON_CONTEXT_SEQ_RM_TYPE_RS ||
                             n_swa > 0);
 
                     bool has_mtmd = false;
